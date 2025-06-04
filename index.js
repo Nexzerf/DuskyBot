1a53f97e5c3ab5b8e0500f5f98fa78f836e56966
周KAIZER <napxswww@gmail.com>
2025-06-04 18:10:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const cookies = process.env.YOUTUBE_COOKIES || '';
+const cookies = process.env.YT_COOKIES || '';
 
 app.get('/', (req, res) => {
   res.send('Bot is alive!');
